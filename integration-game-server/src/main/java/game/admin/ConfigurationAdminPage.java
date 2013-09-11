9a65239aa82f47649a643f23a4478ca98f8b2cbf
Patrick Kranz <patrick@got-email.de>
2013-09-11 08:07:15 +0200

    now it inherits from AdminPage. Makes things easier and we can now change the position of the admin entry with the getGroup method

--- integration-game-server/src/main/java/game/admin/ConfigurationAdminPage.java
+++ integration-game-server/src/main/java/game/admin/ConfigurationAdminPage.java
@@ -1,15 +1,19 @@
 package game.admin;
 
 import game.persistence.BuildConfigurationDao;
+import jetbrains.buildServer.controllers.admin.AdminPage;
 import jetbrains.buildServer.web.openapi.*;
+import org.jetbrains.annotations.NotNull;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
+import static jetbrains.buildServer.web.openapi.PositionConstraint.last;
+
 /**
  * @author Patrick Kranz
  */
-public class ConfigurationAdminPage extends SimpleCustomTab {
+public class ConfigurationAdminPage extends AdminPage {
     public static final String CONFIGURATION_KEY = "configs";
     private static final String JSP_PAGE = "/admin/configuration.jsp";
     private static final String TAB_ID = "configuration";
@@ -19,15 +23,19 @@ public class ConfigurationAdminPage extends SimpleCustomTab {
 
     public ConfigurationAdminPage(PagePlaces pagePlaces, PluginDescriptor pluginDescriptor,
                                   BuildConfigurationDao buildConfigurationDao) {
-        super(pagePlaces, PlaceId.ADMIN_SERVER_CONFIGURATION_TAB, TAB_ID,
-                pluginDescriptor.getPluginResourcesPath(JSP_PAGE), PAGE_TITLE);
+        super(pagePlaces, TAB_ID, pluginDescriptor.getPluginResourcesPath(JSP_PAGE), PAGE_TITLE);
         this.buildConfigurationDao = buildConfigurationDao;
-        setPosition(PositionConstraint.last());
+        setPosition(last());
         register();
     }
 
     @Override
     public void fillModel(Map<String, Object> model, HttpServletRequest request) {
         model.put(CONFIGURATION_KEY, buildConfigurationDao.getConfigurations());
+    }
+
+    @NotNull
+    public String getGroup() {
+        return Groupable.PROJECT_RELATED_GROUP;
     }
 }
