466ec18b681c373132f5b7322bbe072aa3aab702
Patrick Kranz <patrick@got-email.de>
2013-10-24 18:22:40 +0200

    UserScoreDao now also uses the H2 database

--- integration-game-server/src/main/java/game/persistence/UserScoreDao.java
+++ integration-game-server/src/main/java/game/persistence/UserScoreDao.java
@@ -1,56 +1,128 @@
 package game.persistence;
 
 import game.domain.UserScore;
+import jetbrains.buildServer.log.Loggers;
 import jetbrains.buildServer.serverSide.SBuildServer;
-import jetbrains.buildServer.users.PropertyKey;
 import jetbrains.buildServer.users.SUser;
-import jetbrains.buildServer.users.SimplePropertyKey;
 import jetbrains.buildServer.users.UserSet;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import static java.lang.Integer.parseInt;
+import static game.utils.SqlUtils.closeQuietly;
 
 /**
  * @author Patrick Kranz
  */
 public class UserScoreDao {
-    public static final String PROPERTY_KEY = "ci-game-score";
+    private static final String UPDATE_SCORE_SQL =
+            "update Score set Score = ? where UserId = ?";
+    private static final String GET_SCORE_SQL =
+            "select Score From Score where UserId = ?";
+    private static final String INSERT_SCORE_SQL =
+            "insert into Score(UserId, Score) values(?, ?)";
+    private static final String LOAD_SCORE_SQL =
+            "select UserId, Score from Score";
 
-    private final PropertyKey propertyKey;
+
     private final SBuildServer buildServer;
+    private final DataSource dataSource;
 
-    public UserScoreDao(SBuildServer buildServer) {
-        this.propertyKey = new SimplePropertyKey(PROPERTY_KEY);
+    public UserScoreDao(SBuildServer buildServer, DataSource dateSource) {
         this.buildServer = buildServer;
+        this.dataSource = dateSource;
     }
 
-    public void saveScore(SUser user, int score) {
-        user.setUserProperty(propertyKey, Integer.toString(score));
-    }
-
-    public int getScore(SUser user) {
-        String scoreString = user.getPropertyValue(propertyKey);
-        if (scoreString == null) {
-            return 0;
-        } else {
-            return parseInt(scoreString);
+    public void addScore(SUser user, int score) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        try {
+            connection = dataSource.getConnection();
+            statement = connection.prepareStatement(GET_SCORE_SQL);
+            statement.setLong(1, user.getId());
+            ResultSet result = statement.executeQuery();
+            if (result.first()) {
+                int currentScore = result.getInt(1);
+                closeQuietly(statement);
+                statement = connection.prepareStatement(UPDATE_SCORE_SQL);
+                statement.setInt(1, currentScore + score);
+                statement.setLong(2, user.getId());
+                statement.executeUpdate();
+            } else {
+                closeQuietly(statement);
+                statement = connection.prepareStatement(INSERT_SCORE_SQL);
+                statement.setLong(1, user.getId());
+                statement.setInt(2, score);
+                statement.executeUpdate();
+            }
+        } catch (SQLException exception) {
+            Loggers.SQL.error("Error while saving score: ", exception);
+        } finally {
+            closeQuietly(statement);
+            closeQuietly(connection);
         }
     }
 
+/*
+    public int getScore(SUser user) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        try {
+            connection = dataSource.getConnection();
+            statement = connection.prepareStatement(GET_SCORE_SQL);
+            statement.setLong(1, user.getId());
+            ResultSet result = statement.executeQuery();
+            if (result.first()) {
+                return result.getInt(1);
+            }
+        } catch (SQLException exception) {
+            Loggers.SQL.error("Error while executing sql query: ", exception);
+        } finally {
+            closeQuietly(statement);
+            closeQuietly(connection);
+        }
+        return 0;
+    }
+*/
+
     public List<UserScore> getScores() {
         List<UserScore> scores = new ArrayList<UserScore>();
-        UserSet<SUser> userSet = buildServer.getUserModel().getAllUsers();
-        for(SUser user : userSet.getUsers()) {
-            scores.add(new UserScore(user.getUsername(), getScore(user)));
+        Map<Long, String> userMap = getUserIdUserNameMap();
+        Connection connection = null;
+        PreparedStatement statement = null;
+        try {
+            connection = dataSource.getConnection();
+            statement = connection.prepareStatement(LOAD_SCORE_SQL);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Long userId = resultSet.getLong(1);
+                int score = resultSet.getInt(2);
+                if (userMap.containsKey(userId)) {
+                    scores.add(new UserScore(userMap.get(userId), score));
+                }
+            }
+        } catch (SQLException exception) {
+            Loggers.SQL.error("Error while loading user score: ", exception);
+        } finally {
+            closeQuietly(statement);
+            closeQuietly(connection);
         }
         return scores;
     }
 
-    public void addScore(SUser user, int points) {
-        int currentScore = getScore(user);
-        int newScore = currentScore + points;
-        saveScore(user, newScore);
+    private Map<Long, String> getUserIdUserNameMap() {
+        Map<Long, String> userMap = new HashMap<Long, String>();
+        UserSet<SUser> userSet = buildServer.getUserModel().getAllUsers();
+        for(SUser user : userSet.getUsers()) {
+            userMap.put(user.getId(), user.getUsername());
+        }
+        return userMap;
     }
 }
